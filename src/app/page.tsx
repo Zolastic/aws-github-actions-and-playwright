832f323ae295352138f885ad6bb95182f9fd7acd
Zolastic <honhattienlul@gmail.com>
2024-04-13 17:47:52 +0800

    ci: Update AWS configuration in aws.yml to remove environment: production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#fdfbfb] to-[#ebedee] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight text-slate-700 sm:text-[5rem]">
-          AWS GitHub Actions +{" "}
-          <span className="text-slate-400">PlayWright</span> App
+          AWS GitHub Actions <span className="text-slate-400">+</span>{" "}
+          PlayWright <span className="text-slate-400">App</span>
         </h1>
       </div>
     </main>
